7b015f8df33b0c6450a4400b0c076cfdd670472d
Ricky <rontiverosmarquez@gmail.com>
2019-10-31 22:46:54 -0700

    add boostrap 4 and home scren

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   resources :teachers
   resources :schedules
   resources :appointments
+  root to: 'home#main' 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
