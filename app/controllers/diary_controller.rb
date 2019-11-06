3fad498ff8048ef90ce67b982b83a0fd33f0d28c
Ricardo <ricardo@ricardo.com>
2019-11-06 18:52:25 +0000

    delete migrations

--- app/controllers/diary_controller.rb
+++ app/controllers/diary_controller.rb
@@ -1,9 +1,12 @@
 class DiaryController < ApplicationController
     def day 
-        start_date = params[:start_date].nil? ? DateTime.now.beginning_of_day : params[:start_date]
+=begin        
+         start_date = params[:start_date].nil? ? DateTime.now.beginning_of_day : params[:start_date]
         end_date = start_date + 1 
         p start_date
-        @schedules = Schedule.date_range(start_date, end_date)
+        @schedules = Schedule.date_range(start_date, end_date) 
+=end
+        @schedules = Schedule.all
     end
     private
     def student_params
